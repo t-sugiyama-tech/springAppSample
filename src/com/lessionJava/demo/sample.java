c4e965fd0274582045e8f4844e87da855a432f1a
takumi.sugiyama <t-sugiyama@probizmo.co.jp>
2021-01-01 00:37:06 +0900

    #FirstCommit

---
+++ src/com/lessionJava/demo/sample.java
@@ -0,0 +1,11 @@
+package com.lessionJava.demo;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+       int i =0;
+       System.out.print(i);
+	}
+
+}
